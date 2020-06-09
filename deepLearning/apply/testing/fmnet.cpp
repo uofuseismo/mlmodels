2b238c83ecea92333050d7e0e6d7510694c2553b
Ben Baker <bakerb845@gmail.com>
2020-06-09 16:32:41 -0600

    stubbing out fmnet unit test

---
+++ deepLearning/apply/testing/fmnet.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+//#include "uuss/threeComponentPicker/zrunet/processData.hpp"
+//#include "uuss/threeComponentPicker/zrunet/model.hpp"
+#include <gtest/gtest.h>
+
+namespace
+{
+
+TEST(FMNet, Preprocess)
+{
+}
+
+TEST(FMNet, FMNetCPU)
+{
+}
+
+TEST(FMNet, FMNetGPU)
+{
+}
+
+}

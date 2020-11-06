d39daf9b2e9144bbafb2fb75cc9ec88631f39cd1
Ben Baker <bakerb845@gmail.com>
2020-11-06 12:50:41 -0700

    adding pseudo code to make single channel picker

---
+++ deepLearning/apply/include/uuss/oneComponentPicker/zcnn/model.hpp
@@ -0,0 +1,116 @@
+#ifndef UUSSMLMODELS_ONECOMPONENTPICKER_ZCNN_MODEL_HPP
+#define UUSSMLMODELS_ONECOMPONENTPICKER_ZCNN_MODEL_HPP
+#include <memory>
+#include "uuss/enums.hpp"
+namespace UUSS::OneComponentPicker::ZCNN
+{
+/*!
+ * @brief This class is for applying the convolutional neural network
+ *        picker to the vertical channel.  This was architecture
+ *        was defined P Wave Arrival Picking and First-Motion Polarity
+ *        Determination With Deep Learning (Ross et al., 2016).
+ * @copyright Ben Baker (University of Utah) distributed under the MIT license.
+ */
+template<UUSS::Device E = UUSS::Device::CPU>
+class Model
+{
+public:
+    /*! @name Constructors
+     * @{
+     */
+    /*!
+     * @brief Default constructor.
+     */
+    Model();
+    /*! @} */
+
+    /*! @name Destructors
+     * @{
+     */
+    /*!
+     * @brief Destructor.
+     */
+    ~Model();
+    /*! @} */
+
+    /*!
+     * @brief Loads the network weights from an HDF5 file.
+     * @param[in] fileName  The name of the HDF5 file with the model
+     *                      coefficients.
+     * @param[in] verbose   If true then extra information will be printed
+     *                      while loading the weights.  This is useful for
+     *                      debugging.
+     * @throws std::invalid_argument if the HDF5 file does not exist or is
+     *         improperly formatted.
+     */
+    void loadWeightsFromHDF5(const std::string &fileName,
+                             bool verbose = false);
+    /*!
+     * @brief Indicates whether or not the model coefficients are set.
+     * @brief True indicates that the model coefficients are set.
+     */
+    bool haveModelCoefficients() const noexcept;
+    /*!
+     * @result The expected input signal length.
+     * @note The first underlying fully-connected layer requires a flattened
+     *       layer of expected length.  Therefore, the input signal must have
+     *       a specified length as to conform with the model architecture.
+     */
+    int getSignalLength() const noexcept;
+    /*!
+     * @result The assumed sampling p;eriod of the input signal in seconds.
+     */
+    double getSamplingPeriod() const noexcept;
+
+    /*!
+     * @brief Computes the probability of a waveform's polarity as being
+     *        up, down, or unknown.
+     * @param[in] nSignals  The number of signals.  This must be positive.
+     * @param[in] nSamplesInSignal  The number of samples in each signal.
+     *                              This must match \c getSignalLength(). 
+     * @param[in] z   The vertical channel signals from which to compute
+     *                the respective proababilities.  This is a row major
+     *                matrix with dimension [nSignals x nSamplesInSignal].
+     * @param[out] probaUp  The probability of an up polarity for the is'th
+     *                      signal.  This is an array whose dimension is
+     *                      [nSignals].
+     * @param[out] probaDown  The probability of a down polarity for the is'th
+     *                        signal.  This is an array whose dimension is
+     *                        [nSignals].
+     * @param[out] probaUnknown  The probability of unknown polarity for
+     *                           the is'th signal.  This is an array whose
+     *                           dimension is [nSignals].
+     * @param[in] batchSize  The number of signals for torch to process
+     *                       simultaneously.  This must be positive.
+     * @throws std::invalid_argument if nSignals is not positive,
+     *         nSamplesInSignal is not valid, any array is NULL.
+     * @throws std::runtime_error if the coefficients are not set.
+     */
+    void predict(int nSignals, int nSamplesInSignal,
+                 const float z[],
+                 float *pickTimes[],
+                 int batchSize = 32) const;
+    /*! @copydoc predict */
+    void predict(int nSignal, int nSamplesInSignal,
+                  const double z[],
+                  double *pickTimes[],
+                  int batchSize = 32) const;
+    /*!
+     * @brief Computes the pick time in seconds from the trace's start.
+     * @param[in] nSamples   The number of samples in the signal.  
+     *                       This must match \c getSignalLength().
+     * @param[in] z  The vertical channel signal on which to make a P
+     *               pick.
+     * @result The pick time in seconds relative from the trace start. 
+     * @throws std::invalid_argument if nSamples does not equal
+     *         \c getSignalLength() or z is NULL.
+     */
+    float predict(int nSamples, const float z[]) const;
+    /*! @copydoc predict */
+    double predict(int nSamples, const double z[]) const;
+private:
+    class ZCNNImpl;
+    std::unique_ptr<ZCNNImpl> pImpl;
+};
+}
+#endif
---
+++ deepLearning/apply/src/oneComponentPicker/zcnn/model.cpp
@@ -0,0 +1,458 @@
+#include <string>
+#include <torch/torch.h>
+#include "uuss/oneComponentPicker/zcnn/model.hpp"
+#include "private/loadHDF5Weights.hpp"
+
+using namespace UUSS::OneComponentPicker::ZCNN;
+
+namespace
+{
+/*!
+ * @brief Computes the absolute max value of an array.
+ */
+template<class T> T getMaxAbs(const int npts, const T *__restrict__ v)
+{
+#ifdef USE_PSTL
+    auto result = std::minmax_element(std::execution::unseq, v, v+npts);
+#else
+    auto result = std::minmax_element(v, v+npts);
+#endif
+    auto amax = std::max( std::abs(*result.first), std::abs(*result.second) );
+    return amax;
+}
+/*!
+ * @brief Performs a min/max normalization and copies.
+ * @param[in] npts     The number of points
+ * @param[in] z        The vertical trace.  This has dimension [npts].
+ * @param[in] n        The north trace.  This has dimension [npts].
+ * @param[in] e        The east trae.  This has dimension [npts].
+ * @param[out] tensor  The rescaled data for the neural network.
+ *                     This has dimension [3 x npts] and is stored
+ *                     row major format.
+ * @retval True indicates that this is a live trace.
+ * @retval False indicates that this is a dead trace.
+ */
+template<class T>
+bool rescaleAndCopy(const int npts,
+                    const T *__restrict__ z,
+                    T *__restrict__ tensor)
+{
+    auto zmax = getMaxAbs(npts, z);
+    if (zmax != 0)
+    {
+        auto xnorm = 1.0/zmax;
+#ifdef USE_PSTL
+        std::transform(std::execution::unseq,
+                       z, z+npts, tensor,
+                       std::bind(std::multiplies<T>(),
+                       std::placeholders::_1, xnorm));
+#else
+        std::transform(z, z+npts, tensor,
+                       std::bind(std::multiplies<T>(),
+                       std::placeholders::_1, xnorm));
+#endif
+        return true;
+    }
+    else
+    {
+        std::fill(tensor, tensor+npts, 0);
+        return false;
+    }
+}
+
+}
+
+struct FMNetwork : torch::nn::Module
+{
+    // C'tor
+    FMNetwork() :
+        // 1
+        conv1(torch::nn::Conv1dOptions({1, 32, 21})
+           .stride(1).padding(10).bias(true).dilation(1)),
+        batch1(torch::nn::BatchNormOptions(32)
+            .eps(1.e-5).momentum(0.1).affine(true).track_running_stats(true)),
+        // 2
+        conv2(torch::nn::Conv1dOptions({32, 64, 15})
+           .stride(1).padding(7).bias(true).dilation(1)),
+        batch2(torch::nn::BatchNormOptions(64)
+            .eps(1.e-5).momentum(0.1).affine(true).track_running_stats(true)),
+        // 3
+        conv3(torch::nn::Conv1dOptions({64, 128, 11})
+           .stride(1).padding(5).bias(true).dilation(1)),
+        batch3(torch::nn::BatchNormOptions(128)
+            .eps(1.e-5).momentum(0.1).affine(true).track_running_stats(true)),
+        // 4
+        fcn1(torch::nn::LinearOptions({6400, 512}).bias(true)),
+        batch4(torch::nn::BatchNormOptions(512)
+            .eps(1.e-5).momentum(0.1).affine(true).track_running_stats(true)),
+        // 5
+        fcn2(torch::nn::LinearOptions({512, 512}).bias(true)),
+        batch5(torch::nn::BatchNormOptions(512)
+            .eps(1.e-5).momentum(0.1).affine(true).track_running_stats(true)),
+        // 6
+        fcn3(torch::nn::LinearOptions({512, 1}).bias(true))
+    {
+        register_module("conv1d_1", conv1);
+        register_module("bn_1", batch1);
+
+        register_module("conv1d_2", conv2);
+        register_module("bn_2", batch2);
+
+        register_module("conv1d_3", conv3);
+        register_module("bn_3", batch3);
+
+        register_module("fcn_1", fcn1);
+        register_module("bn_4", batch4);
+
+        register_module("fcn_2", fcn2);
+        register_module("bn_5", batch5);
+
+        register_module("fcn_3", fcn3);
+    }
+    /// Forward
+    torch::Tensor forward(const torch::Tensor &xIn)
+    {
+        constexpr int64_t mMaxPoolSize = 2;
+        constexpr int64_t mPoolStride = 2;
+        auto x = conv1->forward(xIn);
+        x = torch::relu(x);
+        x = batch1->forward(x);
+        x = torch::max_pool1d(x, mMaxPoolSize, mPoolStride);
+
+        x = conv2->forward(x);
+        x = torch::relu(x);
+        x = batch2->forward(x);
+        x = torch::max_pool1d(x, mMaxPoolSize, mPoolStride);
+
+        x = conv3->forward(x);
+        x = torch::relu(x);
+        x = batch3->forward(x);
+        x = torch::max_pool1d(x, mMaxPoolSize, mPoolStride);
+
+        x = at::flatten(x, 1);
+
+        x = fcn1->forward(x);
+        x = torch::relu(x);
+        x = batch4->forward(x);
+
+        x = fcn2->forward(x);
+        x = torch::relu(x);
+        x = batch5->forward(x);
+
+        x = fcn3->forward(x);
+        return x;
+    }
+    /// Loads the weights from file
+    void loadWeightsFromHDF5(const std::string &fileName,
+                             const bool gpu = false,
+                             const bool verbose = false)
+    {
+        HDF5Loader loader;
+        loader.openFile(fileName);
+        loader.openGroup("/model_weights/sequential_1"); 
+
+        readWeightsAndBiasFromHDF5(loader, "conv1d_1", conv1, gpu, verbose);
+        readBatchNormalizationWeightsFromHDF5(loader, "bn_1", batch1,
+                                              gpu, verbose);
+
+        readWeightsAndBiasFromHDF5(loader, "conv1d_2", conv2, gpu, verbose);
+        readBatchNormalizationWeightsFromHDF5(loader, "bn_2", batch2,
+                                              gpu, verbose);
+
+        readWeightsAndBiasFromHDF5(loader, "conv1d_3", conv3, gpu, verbose);
+        readBatchNormalizationWeightsFromHDF5(loader, "bn_3", batch3,
+                                              gpu, verbose);
+
+
+        readWeightsAndBiasFromHDF5(loader, "fcn_1", fcn1, gpu, verbose);
+        readBatchNormalizationWeightsFromHDF5(loader, "bn_4", batch4,
+                                              gpu, verbose);
+
+        readWeightsAndBiasFromHDF5(loader, "fcn_2", fcn2, gpu, verbose);
+        readBatchNormalizationWeightsFromHDF5(loader, "bn_5", batch5,
+                                              gpu, verbose);
+
+        readWeightsAndBiasFromHDF5(loader, "fcn_3", fcn3, gpu, verbose);
+
+    }
+//private:
+    torch::nn::Conv1d conv1{nullptr};
+    torch::nn::BatchNorm1d batch1{nullptr};
+    torch::nn::Conv1d conv2{nullptr};
+    torch::nn::BatchNorm1d batch2{nullptr};
+    torch::nn::Conv1d conv3{nullptr};
+    torch::nn::BatchNorm1d batch3{nullptr};
+    torch::nn::Linear fcn1{nullptr};
+    torch::nn::BatchNorm1d batch4{nullptr};
+    torch::nn::Linear fcn2{nullptr};
+    torch::nn::BatchNorm1d batch5{nullptr};
+    torch::nn::Linear fcn3{nullptr};
+};
+
+/// Structure with implementation
+template<UUSS::Device E>
+class Model<E>::ZCNNImpl
+{
+public:
+    void toGPU()
+    {
+        if (!mOnGPU && mUseGPU && mHaveGPU)
+        {
+            mNetwork.to(torch::kCUDA);
+            mOnGPU = true;
+        }
+    }
+    FMNetwork mNetwork;
+    double mSamplingPeriod = 0.01;
+    int mSignalLength = 400;
+    bool mUseGPU = false;
+    bool mOnGPU = false;
+    bool mHaveCoefficients = false;
+    bool mHaveGPU = torch::cuda::is_available();
+};
+
+/// C'tor
+template<>
+Model<UUSS::Device::CPU>::Model() :
+    pImpl(std::make_unique<ZCNNImpl> ())
+{
+    pImpl->mNetwork.eval();
+}
+
+/// C'tor
+template<>
+Model<UUSS::Device::GPU>::Model () :
+    pImpl(std::make_unique<ZCNNImpl> ())
+{
+    if (!torch::cuda::is_available())
+    {
+        std::cerr << "CUDA not available - using CPU" << std::endl;
+    }
+    else
+    {
+        pImpl->mUseGPU = true;
+    }
+    pImpl->toGPU();
+    pImpl->mNetwork.eval();
+}
+
+/// Destructor
+template<UUSS::Device E>
+Model<E>::~Model() = default;
+
+/// Load from weights
+template<UUSS::Device E>
+void Model<E>::loadWeightsFromHDF5(const std::string &fileName,
+                                   const bool verbose)
+{
+    pImpl->mNetwork.loadWeightsFromHDF5(fileName, pImpl->mUseGPU, verbose);
+    if (pImpl->mUseGPU){pImpl->toGPU();}
+    pImpl->mNetwork.eval();
+    pImpl->mHaveCoefficients = true;
+}
+
+/// Returns the input signal length
+template<UUSS::Device E>
+int Model<E>::getSignalLength() const noexcept
+{
+    return pImpl->mSignalLength;
+}
+
+/// Returns the sampling period
+template<UUSS::Device E>
+double Model<E>::getSamplingPeriod() const noexcept
+{
+    return pImpl->mSamplingPeriod;
+}
+
+/// Are the model coefficients set?
+template<UUSS::Device E>
+bool Model<E>::haveModelCoefficients() const noexcept
+{
+    return pImpl->mHaveCoefficients;
+}
+
+/// Predicts a pick time
+template<UUSS::Device E>
+float Model<E>::predict(int nSamples, const float z[]) const
+{
+    std::array<float, 1> pickTime;
+    int nSignals = 1;
+    auto pickTimePtr = pickTime.data();
+    predict(nSignals, nSamples, z, &pickTimePtr);
+    return pickTime[0];
+}
+
+template<UUSS::Device E>
+double Model<E>::predict(const int nSamples, const double z[]) const
+{
+    std::array<double, 1> pickTime;
+    int nSignals = 1;
+    auto pickTimePtr = pickTime.data();
+    predict(nSignals, nSamples, z, &pickTimePtr);
+    return pickTime[0];
+}
+
+/// Compute the pick times relative to the trace start
+template<UUSS::Device E>
+void Model<E>::predict(
+    const int nSignals, const int nSamplesInSignal, const double z[],
+    double *pickTimesIn[], const int batchSize) const
+{
+    // Preliminary checks
+    int signalLength = getSignalLength();
+    if (nSignals < 1){throw std::invalid_argument("No signals");}
+    if (batchSize < 1)
+    {
+        throw std::invalid_argument("Batch size must be positive");
+    }
+    if (nSamplesInSignal != signalLength)
+    {
+        throw std::invalid_argument("nSamplesInSignal = "
+                                  + std::to_string(nSamplesInSignal)
+                                  + " must equal "
+                                  + std::to_string(signalLength));
+    }
+    auto pickTimes = *pickTimesIn;
+    if (pickTimes == nullptr){throw std::invalid_argument("pickTimes is NULL");}
+    // Copy signal to float vector
+    std::vector<float> zWork(nSignals*signalLength, 0);
+    std::copy(z, z + nSignals*signalLength, zWork.data());
+    std::vector<float> pickWork(nSignals, 0);
+    auto pPtr = pickWork.data(); 
+    predict(nSignals, nSamplesInSignal, zWork.data(), &pPtr, batchSize);
+    // Copy result back
+    std::copy(pPtr, pPtr + nSignals, pickTimes);
+}
+    
+/// Compute the pick times relative to the trace start
+template<>
+void Model<UUSS::Device::CPU>::predict(
+    const int nSignals, const int nSamplesInSignal, const float z[],
+    float *pickTimesIn[], const int batchSize) const
+{
+    if (!haveModelCoefficients())
+    {
+        throw std::runtime_error("Model does not have coefficients");
+    }
+    if (nSignals < 1){throw std::invalid_argument("No signals");}
+    if (batchSize < 1)
+    {
+        throw std::invalid_argument("Batch size must be positive");
+    }
+    int signalLength = getSignalLength();
+    if (nSamplesInSignal != signalLength)
+    {
+        throw std::invalid_argument("nSamplesInSignal = "
+                                  + std::to_string(nSamplesInSignal)
+                                  + " must equal "
+                                  + std::to_string(signalLength));
+    }
+    if (z == nullptr){throw std::invalid_argument("z is NULL");}
+    auto pickTimes = *pickTimesIn;
+    if (pickTimes == nullptr){throw std::invalid_argument("pickTimes is NULL");}
+    // Initialize result
+    float defaultPick = static_cast<float> (getSamplingPeriod()/2*signalLength);
+    std::fill(pickTimes, pickTimes + nSignals, defaultPick);
+    // Allocate space
+    int batchUse = std::min(nSignals, batchSize);
+    auto X = torch::zeros({batchUse, 1, signalLength},
+                          torch::TensorOptions().dtype(torch::kFloat32)
+                          .requires_grad(false));
+    std::vector<bool> lAlive(batchUse, true);
+    // Loop on batches
+    float minPick = 0.;
+    float maxPick = getSamplingPeriod()*(signalLength - 1);
+    for (int is0=0; is0<nSignals; is0=is0+batchUse)
+    {
+        // Extract signal, normalize, and copy
+        int js0 = is0;
+        int js1 = std::min(nSignals, js0 + batchUse);
+        for (int js=js0; js<js1; ++js)
+        {
+            int iSrc = js*signalLength;
+            int iDst = (js - js0)*signalLength;
+            float *signalPtr = X.data_ptr<float> () + iDst;
+            lAlive[js-js0] = rescaleAndCopy(signalLength, z + iSrc, signalPtr);
+        }
+        // Predict
+        auto p = pImpl->mNetwork.forward(X);
+        float *pPtr = p.data_ptr<float> ();
+        // Copy probabilities to output arrays
+        for (int js=js0; js<js1; ++js)
+        {
+            if (lAlive[js - js0])
+            {
+                pickTimes[js] = std::min(std::max(minPick, pPtr[js - js0]),
+                                         maxPick);
+            }
+        }
+    }
+}
+
+/// Compute the probability of up/down/unkonwn on a gpu
+template<>
+void Model<UUSS::Device::GPU>::predict(
+    const int nSignals, const int nSamplesInSignal, const float z[],
+    float *pickTimesIn[], const int batchSize) const
+{
+    if (!haveModelCoefficients())
+    {
+        throw std::runtime_error("Model does not have coefficients");
+    }
+    if (nSignals < 1){throw std::invalid_argument("No signals");}
+    if (batchSize < 1)
+    {
+        throw std::invalid_argument("Batch size must be positive");
+    }
+    int signalLength = getSignalLength();
+    if (nSamplesInSignal != signalLength)
+    {
+        throw std::invalid_argument("nSamplesInSignal = "
+                                  + std::to_string(nSamplesInSignal)
+                                  + " must equal "
+                                  + std::to_string(signalLength));
+    }
+    if (z == nullptr){throw std::invalid_argument("z is NULL");}
+    auto pickTimes = *pickTimesIn;
+    if (pickTimes == nullptr){throw std::invalid_argument("pickTimes is NULL");}
+    float defaultPick = static_cast<float> (getSamplingPeriod()/2*signalLength);
+    std::fill(pickTimes, pickTimes + nSignals, defaultPick);
+    int batchUse = std::min(nSignals, batchSize);
+    auto X = torch::zeros({batchUse, 1, signalLength},
+                          torch::TensorOptions().dtype(torch::kFloat32)
+                          .requires_grad(false));
+    std::vector<bool> lAlive(batchUse, true);
+    float minPick = 0.;
+    float maxPick = getSamplingPeriod()*(signalLength - 1);
+    for (int is0=0; is0<nSignals; is0=is0+batchUse)
+    {
+        int js0 = is0;
+        int js1 = std::min(nSignals, js0 + batchUse);
+        for (int js=js0; js<js1; ++js)
+        {
+            int iSrc = js*signalLength;
+            int iDst = (js - js0)*signalLength;
+            float *signalPtr = X.data_ptr<float> () + iDst;
+            lAlive[js-js0] = rescaleAndCopy(signalLength, z + iSrc, signalPtr);
+        }
+        auto signalGPU = X.to(torch::kCUDA);
+        auto pHost = pImpl->mNetwork.forward(signalGPU).to(torch::kCPU);
+        float *pPtr = pHost.data_ptr<float> ();
+        for (int js=js0; js<js1; ++js)
+        {
+            if (lAlive[js - js0])
+            {
+                pickTimes[js] = std::min(std::max(minPick, pPtr[js - js0]),
+                                         maxPick);
+            }
+        }
+    }
+}
+
+
+///--------------------------------------------------------------------------///
+///                              Template Instantiation                      ///
+///--------------------------------------------------------------------------///
+template class UUSS::OneComponentPicker::ZCNN::Model<UUSS::Device::GPU>;
+template class UUSS::OneComponentPicker::ZCNN::Model<UUSS::Device::CPU>;

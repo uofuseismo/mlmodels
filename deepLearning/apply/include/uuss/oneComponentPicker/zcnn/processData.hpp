c7838bf83cb9cdbe00ae99d1b89f87c2b0fcdf05
Ben Baker <bakerb845@gmail.com>
2021-04-08 11:50:35 -0600

    adding preprocessing for one-component picker

---
+++ deepLearning/apply/include/uuss/oneComponentPicker/zcnn/processData.hpp
@@ -0,0 +1,55 @@
+#ifndef UUSSMLMODELS_ONECOMPONENTPICKER_ZCNN_PROCESSDATA_HPP
+#define UUSSMLMODELS_ONECOMPONENTPICKER_ZCNN_PROCESSDATA_HPP
+#include <memory>
+namespace UUSS::OneComponentPicker::ZCNN
+{
+/// @brief This class is for applying a uniform data processing strategy to
+///        the waveform data.  You should call this prior to evaluating
+///        the model.
+/// @copyright Ben Baker (University of Utah) distributed under the MIT license.
+class ProcessData
+{
+public:
+    /// Constructor
+    ProcessData();
+    /// @brief Move constructor.
+    /// @param[in,out] process  The process data class from which to initialize
+    ///                         this class.  On exit, process's behavior is
+    ///                         undefined.
+    ProcessData(ProcessData &&process) noexcept;
+    /// @brief Move assignment.
+    /// @param[in,out] process  The process data class from which to initialize
+    ///                         this class.  On exit, process's behavior is
+    ///                         undefined.
+    /// @result The memory from process moved to this. 
+    ProcessData& operator=(ProcessData &&process) noexcept;
+    /// Destructor
+    ~ProcessData();
+    /// @brief Processes the data.
+    /// @param[in] npts   The number of samples in the time series.
+    /// @param[in] samplingPeriod  The sampling period of the data in seconds.
+    ///                            This must be positive.
+    /// @param[in] data   The data to process.  This is an array whose dimension
+    ///                   is [npts].
+    /// @param[out] processedData  The vector containing the processed data.
+    ///                            The sampling period can be ascertained from
+    ///                            \c getTargetSamplingPeriod().
+    /// @throws std::invalid_argument if npts is not positive, data is NULL,
+    ///         or sampling period is too small.
+    void processWaveform(int npts, double samplingPeriod,
+                         const double data[],
+                         std::vector<double> *processedData);
+    /// @copydoc processWaveform
+    void processWaveform(int npts, double samplingPeriod,
+                         const float data[],
+                         std::vector<float> *processedData);
+    /// @result The sampling period of the processed signals.
+    [[nodiscard]] double getTargetSamplingPeriod() const;
+    ProcessData(const ProcessData &p) = delete;
+    ProcessData& operator=(const ProcessData &p) = delete;
+private:
+    class ProcessDataImpl;
+    std::unique_ptr<ProcessDataImpl> pImpl;
+};
+}
+#endif
---
+++ deepLearning/apply/src/oneComponentPicker/zcnn/processData.cpp
@@ -0,0 +1,131 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <rtseis/postProcessing/singleChannel/waveform.hpp>
+#include "uuss/oneComponentPicker/zcnn/processData.hpp"
+
+using namespace UUSS::OneComponentPicker::ZCNN;
+
+class ProcessData::ProcessDataImpl
+{
+public:
+    /// Holds the data processing waveform
+    RTSeis::PostProcessing::SingleChannel::Waveform<double> mWave;
+    /// Anticipating 100 Hz sampling rate
+    double mTargetSamplingPeriod = 0.01;
+    /// Remove trend
+    bool mRemoveTrend = true;
+    /// Taper the data prior to filtering
+    double mTaperPct = 1;
+    bool mDoTaper = true;
+    /// Bandpass filter data
+    int mFilterPoles = 2;
+    std::pair<double, double> mCorners = {1, 20}; // 1 Hz to 17 Hz
+    RTSeis::PostProcessing::SingleChannel::IIRPrototype mPrototype
+        = RTSeis::PostProcessing::SingleChannel::IIRPrototype::BUTTERWORTH;
+    double mRipple = 5; // Ripple for Chebyshev 1 or 2 - not applicable
+    const bool mZeroPhase = false; // Filter has to be applied causally
+    // This is not for real-time
+    RTSeis::PostProcessing::SingleChannel::InterpolationMethod  mInterpolationMethod
+        = RTSeis::PostProcessing::SingleChannel::InterpolationMethod::DFT;
+};
+
+/// Constructor
+ProcessData::ProcessData() :
+    pImpl(std::make_unique<ProcessDataImpl> ())
+{
+}
+
+/// Move c'tor
+ProcessData::ProcessData(ProcessData &&process) noexcept
+{
+    *this = std::move(process);
+}
+
+/// Move assignment
+ProcessData& ProcessData::operator=(ProcessData &&process) noexcept
+{
+    if (&process == this){return *this;}
+    pImpl = std::move(process.pImpl);
+    return *this;
+}
+
+/// Destructor
+ProcessData::~ProcessData() = default;
+
+/// Processes the data
+void ProcessData::processWaveform(
+    const int npts,
+    const double samplingPeriod,
+    const float data[],
+    std::vector<float> *processedData)
+{
+    std::vector<double> dataIn(npts);
+    #pragma omp simd
+    for (int i=0; i<npts; ++i){dataIn[i] = static_cast<double> (data[i]);}
+    std::vector<double> temp;
+    processWaveform(npts, samplingPeriod, dataIn.data(), &temp);
+    int nptsNew = static_cast<int> (temp.size());
+    processedData->resize(nptsNew, 0);
+    auto dPtr = processedData->data();
+    #pragma omp simd
+    for (int i=0; i<nptsNew; ++i){dPtr[i] = static_cast<float> (temp[i]);}
+}
+
+/// Processes the data
+void ProcessData::processWaveform(
+    const int npts,
+    const double samplingPeriod,
+    const double data[],
+    std::vector<double> *processedData)
+{
+    if (samplingPeriod <= 0)
+    {
+        throw std::invalid_argument("Sampling period = "
+                                  + std::to_string(samplingPeriod)
+                                  + "must be positive");
+    }
+    auto fnyq = 1/(2*samplingPeriod);
+    if (fnyq < pImpl->mCorners.second)
+    {
+        throw std::invalid_argument("High corner exceeds Nyquist");
+    }
+    if (npts < 1 || data == nullptr)
+    {
+        if (npts < 1){throw std::invalid_argument("No data points");}
+        throw std::invalid_argument("Data is NULL");
+    }
+    // Set the data
+    pImpl->mWave.setData(npts, data);
+    pImpl->mWave.setSamplingPeriod(samplingPeriod);
+    // Process the data
+    // (1) Remove trend
+    if (pImpl->mRemoveTrend){pImpl->mWave.detrend();}
+    // (2) Taper to minimize edge effects near filter start
+    if (pImpl->mDoTaper){pImpl->mWave.taper(pImpl->mTaperPct);}
+    // (3) Filter: Note that the bandpass will prevent aliasing
+    //     when downsampling the 200 Hz stations.
+    pImpl->mWave.iirBandpassFilter(pImpl->mFilterPoles,
+                                   pImpl->mCorners,
+                                   pImpl->mPrototype,
+                                   pImpl->mRipple,
+                                   pImpl->mZeroPhase);
+    // (4) Potentially esample to target sampling period (e.g., 100 Hz)
+    if (std::abs(samplingPeriod - pImpl->mTargetSamplingPeriod) > 1.e-5)
+    {
+        pImpl->mWave.interpolate(pImpl->mTargetSamplingPeriod,
+                                 pImpl->mInterpolationMethod);
+    }
+    // Get the data
+    auto nNewSamples = pImpl->mWave.getOutputLength();
+    processedData->resize(nNewSamples);
+    double *dataPtr = processedData->data();
+    pImpl->mWave.getData(nNewSamples, &dataPtr);
+}
+
+/// Get sampling period
+double ProcessData::getTargetSamplingPeriod() const
+{
+    return pImpl->mTargetSamplingPeriod;
+}
+

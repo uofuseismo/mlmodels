d801c86b577204c47438f0742e4f1e210b31c401
Ben Baker <bakerb845@gmail.com>
2021-06-21 10:30:58 -0600

    adding test to picker

--- deepLearning/apply/testing/zrcnn.cpp
+++ deepLearning/apply/testing/zrcnn.cpp
@@ -51,10 +51,54 @@ TEST(ZCNN, Picker)
     UUSS::OneComponentPicker::ZCNN::Model picker;
     EXPECT_NO_THROW(picker.loadWeightsFromHDF5(
         "../testing/models/test_zrcnnpick_p.h5"));
-std::cout << picker.predict(ex1.size(), ex1.data()) << std::endl;
-std::cout << picker.predict(ex2.size(), ex2.data()) << std::endl;
-std::cout << picker.predict(ex3.size(), ex3.data()) << std::endl;
-std::cout << picker.predict(ex4.size(), ex4.data()) << std::endl;
+    auto p1 = picker.predict(ex1.size(), ex1.data());
+    auto p2 = picker.predict(ex2.size(), ex2.data());
+    auto p3 = picker.predict(ex3.size(), ex3.data());
+    auto p4 = picker.predict(ex4.size(), ex4.data());
+    EXPECT_NEAR(0.3296588,  p1, 1.e-5);
+    EXPECT_NEAR(0.29644084, p2, 1.e-5);
+    EXPECT_NEAR(0.43443173, p3, 1.e-5);
+    EXPECT_NEAR(0.44393498, p4, 1.e-5);
+    std::vector<double> reference({0.3296588, 0.29644084,
+                                   0.43443173, 0.44393498});
+    int nSignals = 36;
+    int batchSize = 32;
+    std::vector<double> signals(nSignals*ex1.size(), 0);
+    for (int i = 0; i < nSignals; ++i)
+    {
+        auto i1 = i*ex1.size();
+        if (i%4 == 0)
+        {
+            std::copy(ex1.data(), ex1.data() + ex1.size(),
+                      signals.data() + i1);  
+        }
+        else if (i%4 == 1)
+        {
+            std::copy(ex2.data(), ex2.data() + ex2.size(),
+                      signals.data() + i1);
+        }
+        else if (i%4 == 2)
+        {
+            std::copy(ex3.data(), ex3.data() + ex3.size(),
+                      signals.data() + i1);
+        }
+        else if (i%4 == 3)
+        {
+            std::copy(ex4.data(), ex4.data() + ex4.size(),
+                      signals.data() + i1);
+        }
+        else
+        {
+            EXPECT_TRUE(false);
+        } 
+    }
+    std::vector<double> perts(nSignals);
+    auto pPtr = perts.data();
+    picker.predict(nSignals, ex1.size(), signals.data(), &pPtr, batchSize); 
+    for (int i = 0; i < nSignals; ++i)
+    {
+        EXPECT_NEAR(reference[i%4], perts[i], 1.e-5);
+    }
 }
 
 }

bc4fcca6a937c637d33467a96c8b1eb8d34e0b68
Ben Baker <bakerb845@gmail.com>
2023-09-07 16:44:55 -0600

    forgot a header

---
+++ python/eventClassifiers.hpp
@@ -0,0 +1,11 @@
+#ifndef UUSS_PYTHON_EVENT_CLASSIFIERS_HPP
+#define UUSS_PYTHON_EVENT_CLASSIFIERS_HPP
+#include <vector>
+#include <memory>
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+namespace UUSSMLModels::Python::EventClassifiers
+{
+
+}
+#endif
